4eb81ccfb5d8bd99dbfd60055fa80bf2018fb620
Thomas Pouvreau <contact@thomas-pouvreau.fr>
2020-05-29 15:35:48 +0200

    basic setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ipyreactwidgets",
+    version="0.0.1",
+    author="weatherforce",
+    author_email="opensource@weatherforce.org",
+    description="A widget library, based on React",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="git@gitlab.com:weatherforce-platform/ipyreactwidgets.git",
+    data_files=[
+        # like jupyter nbextension install --sys-prefix
+        # like jupyter nbextension enable --sys-prefix
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
