c27899e7b58383dada0e7caff4576dddb9618c69
Thomas Pouvreau <contact@thomas-pouvreau.fr>
2020-06-02 15:40:54 +0200

    made it work with ES6

--- nbextensions_srcs/bin/post_build.js
+++ nbextensions_srcs/bin/post_build.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
+
+const getTemplate = ()=>{
+	return JSON.parse(fs.readFileSync('template.json'))
+}
 //grab provided args
 const [,, ...args] = process.argv
 
-console.log(`hello world ${args}`)
+
+
+console.log(getTemplate())
