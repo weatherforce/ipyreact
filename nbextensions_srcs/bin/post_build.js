9ce96c32a319adc06fa28c0208ad1385c213763b
Thomas Pouvreau <contact@thomas-pouvreau.fr>
2020-06-02 11:47:51 +0200

    Divide nbextension sources from result/ start development of a post-build script

---
+++ nbextensions_srcs/bin/post_build.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+//grab provided args
+const [,, ...args] = process.argv
+
+console.log(`hello world ${args}`)
--- nbextensions_srcs/webpack.config.js
+++ nbextensions_srcs/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+	entry:{
+		widget: './widget/src/index.js'	
+	},
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, '../widget/static'),
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../nbextensions_dists/widget'),
         libraryTarget: 'amd',
     },
     module:{
