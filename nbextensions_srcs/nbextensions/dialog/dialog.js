71d6c53139cfb7146fd387c7bcb65c335b84ec03
Thomas Pouvreau <contact@thomas-pouvreau.fr>
2020-06-10 15:52:33 +0200

    made example code from material ui work on Jupyter nb

--- nbextensions_srcs/nbextensions/dialog/dialog.js
+++ nbextensions_srcs/nbextensions/dialog/dialog.js
@@ -1,14 +1,62 @@
 import React from 'react'
 import Widget from '../../utils/widget/widget'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
-
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 
 class IDialog extends Widget {
 
-	render(){
-		return(<h1>Hello World </h1>)
+	constructor(props){
+		super(props);
+		this.state = {open: false}
+		this.handleClickOpen = this.handleClickOpen.bind(this)
+		this.handleClickClose = this.handleClickClose.bind(this)
 	}
+
+	handleClickOpen(event){
+		this.setState({open:true})	
+	}
+
+	handleClickClose(event){
+		this.setState({open:false})	
+	}
+
+	render(){
+		return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Dialog
+        open={this.state.open}
+        onClose={this.handleClickClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous location data to
+            Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClickClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={this.handleClickClose} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
 }
 
 export default IDialog 
