8495045f0ec6ef9adea93faeecc20490a08cc328
Thomas Pouvreau <contact@thomas-pouvreau.fr>
2020-06-16 16:07:40 +0200

    Add title and content as arguments to Dialog widget

--- ipyreactwidgets/dialog.py
+++ ipyreactwidgets/dialog.py
@@ -3,6 +3,6 @@ from ipyreactwidgets import Widget
 
 class Dialog(Widget):
 
-    def __init__(self):
+    def __init__(self, title, content):
         super().__init__("dialog")
-        self.state = {"open": False}
+        self.state = {"open": False, "title": title, "content": content}
--- nbextensions_srcs/nbextensions/dialog/dialog.js
+++ nbextensions_srcs/nbextensions/dialog/dialog.js
@@ -42,11 +42,10 @@ class IDialog extends Widget {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogTitle id="alert-dialog-title">{this.state.title}</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous location data to
-            Google, even when no apps are running.
+			{this.state.content}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
