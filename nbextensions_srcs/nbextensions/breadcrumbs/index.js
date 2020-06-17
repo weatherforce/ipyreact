818e9748a3c4a0d97cd5e91f7a957e3ea2d324b5
Thomas Pouvreau <contact@thomas-pouvreau.fr>
2020-06-17 15:23:03 +0200

    fix issue by providing a widgetName variable

--- nbextensions_srcs/nbextensions/breadcrumbs/index.js
+++ nbextensions_srcs/nbextensions/breadcrumbs/index.js
@@ -3,5 +3,5 @@ import {CommWrapper} from '../../utils/commwrapper/commWrapper'
 import BreadCrumbs from './breadcrumbs'
 
 export const load_ipython_extension = () => {
-	CommWrapper("breadcrumbs_comm", BreadCrumbs)
+	CommWrapper("breadcrumbs", BreadCrumbs)
 }
