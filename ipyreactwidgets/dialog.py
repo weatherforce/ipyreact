f8f9c1a7c286890dfb2d1d1fa7dc9a0c75ca0bc5
Thomas Pouvreau <contact@thomas-pouvreau.fr>
2020-06-18 16:00:43 +0200

    add State class as a wrapp around dict so we can trigger send_updates everytime state is updated

--- ipyreactwidgets/dialog.py
+++ ipyreactwidgets/dialog.py
@@ -5,4 +5,6 @@ class Dialog(Widget):
 
     def __init__(self, title, content):
         super().__init__("dialog")
-        self.state = {"open": False, "title": title, "content": content}
+        self.state["open"] = False
+        self.state["title"] = title,
+        self.state["content"] = content
