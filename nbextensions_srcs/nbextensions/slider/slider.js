f9abe22a12af6f0fd64fac86a1005544c6da0a7e
Alex Blaza <alex.blaza@weatherforce.org>
2020-06-24 15:20:40 +0200

    Apply JavaScript Standard Style

--- nbextensions_srcs/nbextensions/slider/slider.js
+++ nbextensions_srcs/nbextensions/slider/slider.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import Widget from '../../utils/widget/widget';
-import Grid from '@material-ui/core/Grid';
-import SliderReact from '@material-ui/core/Slider';
-
+import React from 'react'
+import Widget from '../../utils/widget/widget'
+import Grid from '@material-ui/core/Grid'
+import SliderReact from '@material-ui/core/Slider'
 
 class Slider extends Widget {
+  constructor (props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-	constructor(props){
-		super(props)
-		this.handleChange = this.handleChange.bind(this)
-	}
+  handleChange (event, newValue) {
+    this.setState({ value: newValue })
+  }
 
-	handleChange(event, newValue){
-			this.setState({"value": newValue});
-	}
-
-  render() {
-    let value = this.state.value;
+  render () {
+    const value = this.state.value
 
     return (
       <div>
         <Grid item xs>
-          <SliderReact value={value} onChange={this.handleChange} aria-labelledby="continuous-slider" />
+          <SliderReact value={value} onChange={this.handleChange} aria-labelledby='continuous-slider' />
         </Grid>
       </div>
-    );
-}
+    )
+  }
 }
 
 export default Slider
--- nbextensions_srcs/utils/widget/widget.js
+++ nbextensions_srcs/utils/widget/widget.js
@@ -1,38 +1,35 @@
 import React from 'react'
 
-
-class widget extends React.Component{
-
-    constructor(props){
-        super(props)
-        this.state = props.state
-		this.processChildren = this.processChildren.bind(this)
-		this.children = this.processChildren(props.children)
-    }
-
-  componentDidUpdate(){
-    let data ={"state": this.state}
-    this.props.comm.send(data);
+class widget extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = props.state
+    this.processChildren = this.processChildren.bind(this)
+    this.children = this.processChildren(props.children)
   }
 
-	processChildren(children){
-		if (children){
-			return children.map( (child) => {
-				return window.widgetRegistry[child] ? window.widgetRegistry[child] : "void"
-			})
-		}
-		return ["void"]
-	}
+  componentDidUpdate () {
+    const data = { state: this.state }
+    this.props.comm.send(data)
+  }
 
+  processChildren (children) {
+    if (children) {
+      return children.map((child) => {
+        return window.widgetRegistry[child] ? window.widgetRegistry[child] : 'void'
+      })
+    }
+    return ['void']
+  }
 
-    render(){
-        return(
-        <div>
-            <h1> Hello World </h1>
-			<p>{JSON.stringify(this.state)}</p>
-        </div>
-		)
-	}
+  render () {
+    return (
+      <div>
+        <h1> Hello World </h1>
+        <p>{JSON.stringify(this.state)}</p>
+      </div>
+    )
+  }
 }
 
 export default widget
