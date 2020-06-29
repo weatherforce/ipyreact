6e90c48c9594da7c5062d4255006618b859fd93c
Thomas Pouvreau <contact@thomas-pouvreau.fr>
2020-06-29 16:21:18 +0200

    change camelCase to more_pythonic_method_name on widget.py

--- ipyreactwidgets/widget.py
+++ ipyreactwidgets/widget.py
@@ -11,16 +11,16 @@ class Widget(HasTraits):
         self.target_name = f"{self.widget_id}_comm"
         self.props = {}
         self.children = children
-        self.setcommunication()
+        self.set_communication()
 
-    def processChildren(self):
+    def process_children(self):
         return [a_child.widget_id for a_child in self.children]
 
-    def setcommunication(self, render="default"):
+    def set_communication(self, render="default"):
         data = {"state": self.state,
                 "props": self.props,
                 "render": render,
-                "children": self.processChildren()
+                "children": self.process_children()
                 }
         self.communication = Comm(target_name=self.target_name, data=data)
         self.communication.on_msg(self._received)
@@ -40,4 +40,4 @@ class Widget(HasTraits):
         self.communication.send(data)
 
     def _ipython_display_(self, **kwargs):
-        self.setcommunication(render="cell")
+        self.set_communication(render="cell")
