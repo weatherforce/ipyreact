4eef7dfdb68fc69a40c71a7b49fb55188e5c8bfb
Thomas Pouvreau <contact@thomas-pouvreau.fr>
2020-07-07 11:14:45 +0200

    Handle value changes

--- ipyreact/input_field.py
+++ ipyreact/input_field.py
@@ -3,5 +3,7 @@ from ipyreact import Widget
 
 class InputField(Widget):
 
-    def __init__(self):
-        super().__init__("inputfield")
+    def __init__(self, input_type="text"):
+        props = {"type": input_type}
+        super().__init__("inputfield", props=props)
+        self.state = {"value": 0}
--- nbextensions_srcs/nbextensions/inputfield/inputfield.js
+++ nbextensions_srcs/nbextensions/inputfield/inputfield.js
@@ -1,9 +1,19 @@
 import Widget from '../../utils/widget/widget'
 
 class InputField extends Widget{
+	constructor(props){
+		super(props)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(event){
+		const value = event.target.value
+		this.setState({"value": value})
+	}
+
 	render(){
 		return(
-			<input type="text" id="name" name="name" required minlength="4" maxlength="8" size="10"/>
+			<input type={this.props.type} value={this.state.value}/>
 		)
 	}
 }
