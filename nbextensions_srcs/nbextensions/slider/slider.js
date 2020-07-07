301ba306514326e54437726c9253b0ef3b295e8d
Alex Blaza <alex.blaza@weatherforce.org>
2020-07-07 15:08:59 +0200

    Fix state update issue in the slider

--- nbextensions_srcs/nbextensions/slider/slider.js
+++ nbextensions_srcs/nbextensions/slider/slider.js
@@ -1,32 +1,31 @@
 import Widget from '../../utils/widget/widget'
 
-
 class Slider extends Widget {
-  constructor(props) {
-    super(props);
-    this.updateRange = this.updateRange.bind(this)
+  constructor (props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  updateRange(event) {
+  handleChange (event) {
     const rangeValue = event.target.value
     this.setState({
       value: parseInt(rangeValue)
     })
   }
- 
-  render () {
 
+  render () {
     return (
 
       <div>
-        <input id="range" type="range"
-          value={this.value}
-          min="0"
-          max="20"
-          step="1"
-          onChange={this.updateRange}
+        <input
+          id='range' type='range'
+          value={this.state.value}
+          min='0'
+          max='20'
+          step='1'
+          onChange={this.handleChange}
         />
-        <span id="output">{this.value}</span>
+        <span id='output'>{this.state.value}</span>
       </div>
     )
   }
