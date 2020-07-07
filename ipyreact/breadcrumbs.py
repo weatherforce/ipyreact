865f0ae74b78b5afcdcf1c45fc1825233c332187
Alex Blaza <alex.blaza@weatherforce.org>
2020-07-07 16:35:49 +0200

    Remove material UI widgets material UI widgets can be found if needed in git history- widgets removed due to us wanting to only have fully functioing widgets as examples

--- ipyreact/breadcrumbs.py
+++
@@ -1,8 +0,0 @@
-from ipyreact import Widget
-
-
-class BreadCrumbs(Widget):
-
-    def __init__(self, pathlist: list):
-        super().__init__("breadcrumbs")
-        self.state["pathlist"] = pathlist
--- nbextensions_srcs/nbextensions/breadcrumbs/index.js
+++
@@ -1,6 +0,0 @@
-import CommWrapper from '../../utils/commwrapper/commWrapper'
-import BreadCrumbs from './breadcrumbs'
-
-export const load_ipython_extension = () => {
-  CommWrapper('breadcrumbs', BreadCrumbs)
-}
