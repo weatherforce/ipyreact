ef8d42c8bde1138b3f7facc933a793b0ca69b8f1
Thomas Pouvreau <contact@thomas-pouvreau.fr>
2020-07-08 14:44:48 +0200

    run standard

--- nbextensions_srcs/nbextensions/label/label.js
+++ nbextensions_srcs/nbextensions/label/label.js
@@ -1,10 +1,6 @@
 import Widget from '../../utils/widget/widget'
 
 class Label extends Widget {
-  constructor (props) {
-    super(props)
-  }
-
   render () {
     return (
       <label> {this.state.content} </label>
